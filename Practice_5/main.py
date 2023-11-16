a95663d553c9bb0a93ada8bb027970a3add50450
shokii NoteBook <shilo152004@mail.ru>
2023-11-16 19:56:59 +0300

    prac 5

--- Practice_5/main.py
+++ Practice_5/main.py
@@ -1,20 +1,74 @@
-import os
-import subprocess
+import json
+import requests
+import sys
+import webbrowser
 
-def visualize_commit_graph(dir):
-    git_dir = os.path.join(dir, ".git")
 
-    if not os.path.exists(git_dir):
-        print("Папка .git не найдена.")
-        return
+def getGraphizForLib(requiresDist, libName):
+    allPath = ""
+    nextLine = "%3B%0A%20%20"
+    if requiresDist == None:
+        allPath += libName
+    else:
+        for item in requiresDist:
+            if (item != libName):
+                allPath += libName + "->" + str(item) + nextLine
+    return allPath
 
-    cmd = ["git", "--git-dir=" + git_dir, "log", "--all", "--oneline", "--graph"]
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    commit_logs = result.stdout.strip().split("\n")
 
-    for commit_log in commit_logs:
-        print(commit_log)
+def getRequiresDist(libName):
+    mask = "https://pypi.org/pypi/"
+    mask2 = "/json"
+    url = mask + libName + mask2
+    site = requests.get(url=url)
+    if (site.status_code == 404):
+        print("The library with this name could not be obtained! Enter another name.")
+        return None
+    jsonFile = json.loads(site.text)
+    return jsonFile
 
-# Direction for parse
-dir = "C:\\Users\\shilo\\Документы\\GIT\\SIAOD"
-visualize_commit_graph(dir)
+
+def formatingRequires(requiresDist):
+    if not (requiresDist == None):
+        for i in range(0, len(requiresDist)):
+            requiresDist[i] = \
+            str(requiresDist[i]).split('>', 1)[0].split('<', 1)[0].split("extra", 1)[0].split('[', 1)[0].split(';', 1)[
+                0].split('=', 1)[0].split('(', 1)[0].split(' ', 1)[0].split('.', 1)[0]
+            requiresDist[i] = str(requiresDist[i]).replace('-', '_')
+    return requiresDist
+
+
+def childRequiresDist(requiresDist, childPath="", ):
+    if requiresDist != None:
+        for item in requiresDist:
+            jsonFile = getRequiresDist(str(item))
+            if (jsonFile != None):
+                requiresDist = formatingRequires(jsonFile['info']['requires_dist'])
+                libName = jsonFile['info']['name']
+                childPath += getGraphizForLib(requiresDist, libName)
+                childRequiresDist(requiresDist, childPath)
+    return childPath
+
+
+if __name__ == "__main__":
+    while True:
+        if "--script" != sys.argv[1]:
+            print("No such command in script")
+            exit(-1)
+        else:
+            ## ЭТАП ПОДКЛЮЧЕНИЯ К САЙТУ
+            graphizSite = "https://dreampuf.github.io/GraphvizOnline/#digraph%20G%20%7B%0A%20%20%20%20"
+            graphizSite2 = "%0A%7D"
+            allPat = ""
+            nextName = ""
+            libName = sys.argv[2]
+
+            ## ЭТАП СОЗДАНИЯ JSON
+            jsonFile = getRequiresDist(libName)
+            requiresDist = jsonFile['info']['requires_dist']
+            libName = jsonFile['info']['name']
+            requiresDist = formatingRequires(requiresDist)  # ФОРМАТИРОВАНИЕ ТЕКСТА В УДОБНЫЙ ФОРМАТ
+            siteReq = graphizSite + getGraphizForLib(requiresDist, libName) + graphizSite2
+            print(siteReq)
+            webbrowser.open(siteReq)
+            exit(0)
